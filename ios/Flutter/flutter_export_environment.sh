91829cad377b76562f2135ca2deff7e68f40d302
Pratik Singhal <singhalpratik037@gmail.com>
2020-10-02 10:10:44 +0530

    local changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/pratik037/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/pratik037/Flutter Projects/fluttercrypto"
+export "FLUTTER_ROOT=/home/pratik037/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pratik037/development/flutter projects/fluttercrypto"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/pratik037/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/pratik037/snap/flutter/common/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
